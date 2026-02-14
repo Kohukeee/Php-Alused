06c350325b9201ca2b15479d36a198f5b74f8035
Kohukeee <hkoplimaa@hkhk.edu.ee>
2026-02-14 09:55:27 +0200

    asd

--- h01.php
+++ h01.php
@@ -19,6 +19,22 @@
         echo "<br>";
         echo '"It\'s my Life" - Dr. Alban';
     ?>
+    <h2>Harjutus 02 - Vormid</h2>
+    <form action="h01.php" class="get">
+        Arv1: <input type="number" name="Arv1"><br>
+        Arv2: <input type="number" name="Arv2"><br>
+        <input type="submit" value="Liida"><br>
+    </form>
+    
+    <?php
+        if(!empty($nr1 = $_GET["Arv1"]) && !empty($nr2 = $_GET["Arv2"])){
+            $nr1 = $_GET["Arv1"];
+            $nr2 = $_GET["Arv2"];
+            $liida = $nr1 + $nr2;
+
+            echo $liida;
+        }
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
